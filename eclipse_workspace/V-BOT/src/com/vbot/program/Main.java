c634dc053db01a0f89da1080eade44819f90197e
Marek <marek@localhost.localdomain>
2021-07-24 22:18:48 +0200

    added Token-Manager to load token from local file

---
+++ eclipse_workspace/V-BOT/src/com/vbot/program/Main.java
@@ -0,0 +1,11 @@
+package com.vbot.program;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		final String TOKEN = TokenManager.getToken();
+		
+	}
+
+}
---
+++ eclipse_workspace/V-BOT/src/com/vbot/program/TokenManager.java
@@ -0,0 +1,31 @@
+package com.vbot.program;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TokenManager {
+
+	public static String getToken() {
+		
+		// Define local token path
+		File file = new File("/home/vbot/token.txt");
+		
+		// Declare Scanner
+		Scanner scanner = null;
+		
+		// Create scanner
+		try {
+			scanner = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// Read token from file
+		String token = scanner.nextLine();
+		
+		return token;
+		
+	}
+	
+}
